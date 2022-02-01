8fbda79f95ec47cb7da80cb403ddf63ab0cfebc0
William <ochowei@gmail>
2022-02-01 19:20:48 +0800

    Add storybook-like app by create-react-app

---
+++ src/App.js
@@ -0,0 +1,36 @@
+// in src/App.js
+import * as React from "react";
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  Create,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+  NumberInput,
+} from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { IntegerInput } from "./IntegerInput";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <NumberInput source="id"></NumberInput>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+      <IntegerInput source="example2" />
+    </SimpleForm>
+  </Create>
+);
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} create={PostCreate} />
+  </Admin>
+);
+export default App;
